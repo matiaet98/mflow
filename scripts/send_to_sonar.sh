5f87ad0edba8d6988187d3f4eec7dfb293729ed6
Matias Estevez <matias.exequiel.estevez@gmail.com>
2019-07-13 20:37:07 -0300

    gitignore y scripts

---
+++ scripts/send_to_sonar.sh
@@ -0,0 +1,5 @@
+/opt/sonar/bin/sonar-scanner \
+    -Dsonar.projectKey=siper \
+    -Dsonar.sources=. \
+    -Dsonar.host.url=http://localhost:9000 \
+    -Dsonar.login=f0bdfd1514eebf9c10336a60c9cd7dacc61f0531
