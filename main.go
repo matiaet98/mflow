850e073fe6e60e94803f69e1034f5fbc763b1e4c
Matias Estevez <matias.exequiel.estevez@gmail.com>
2019-07-13 15:15:40 -0300

    pid config

--- main.go
+++ main.go
@@ -22,11 +22,25 @@ func init() {
 		os.Exit(1) //Salgo con error porque no pude ni leer la config
 	}
 	pidfile.SetPidfilePath(Config.Global.PidDirectory + pidFilename)
+	pid, err := pidfile.Read()
+	if err == nil { //Significa que ya existe un PID
+		fmt.Println("Error: El proceso ya esta corriendo (ya existe un PID)")
+		fmt.Println("PID: ", pid)
+		os.Exit(1)
+	}
 	err = pidfile.Write()
 	if err != nil {
 		fmt.Println("Error al crear PID: " + err.Error())
-		os.Exit(1) //Salgo con error porque no pude ni leer la config
+		os.Exit(1)
 	}
+}
+
+func salir() {
+	err := os.Remove(pidfile.GetPidfilePath())
+	if err != nil {
+		fmt.Println("Error: Error removiendo pid file: ", err.Error())
+	}
+	os.Exit(0)
 }
 
 func signalCatcher() {
@@ -34,13 +48,17 @@ func signalCatcher() {
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	<-ch
 	fmt.Println("Presiono CTRL-C; saliendooo")
-	os.Exit(0)
+	salir()
 }
 
 func main() {
 	go signalCatcher()
 	for {
 		Conf, err := config.ReadConfig()
+		if err != nil {
+			fmt.Println("Error: Revise la configuracion")
+			os.Exit(1)
+		}
 		time.Sleep(time.Second * time.Duration(Conf.Global.CheckNewConfigInterval))
 		for _, value := range Conf.Tasks {
 			fmt.Println(value.ID)
