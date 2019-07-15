94876dd99e9bb0d0bd7f2a0f4bcc71038495be82
Matias Estevez <matias.exequiel.estevez@gmail.com>
2019-07-15 00:19:49 -0300

    corrida de tareas pendientes unicamente

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -54,8 +54,29 @@ func runTask(task config.Task, sem chan bool) {
 
 //RunTasks : Corre todas las tareas del slice que recibe
 func RunTasks(AllTasks []config.Task, maxParallel int) {
-	sem := make(chan bool, maxParallel)
+	var PendingTasks []config.Task
 	for _, task := range AllTasks {
+		status, fecha, err := getTaskStatus(task.ID)
+		if err != nil {
+			fmt.Println("Error: ", err) //no la agrego si hay error
+			continue
+		}
+		if status == "RUNNING" {
+			continue
+		}
+		if status == "NONE" {
+			PendingTasks = append(PendingTasks, task)
+			continue
+		}
+		year, month, day := time.Now().Date()
+		if int(fecha.Sub(time.Date(year, month, day, 0, 0, 0, 0, time.UTC))) < 0 { //si es anterior a hoy
+			PendingTasks = append(PendingTasks, task)
+			continue
+		}
+	}
+
+	sem := make(chan bool, maxParallel)
+	for _, task := range PendingTasks {
 		sem <- true
 		go runTask(task, sem)
 	}
@@ -64,36 +85,38 @@ func RunTasks(AllTasks []config.Task, maxParallel int) {
 	}
 }
 
-func getTaskStatus(ID int) (string, error) {
+func getTaskStatus(ID int) (string, time.Time, error) {
 	db, err := sql.Open("goracle", config.Config.EtlUser+"/"+config.Config.EtlPassword+"@"+config.Config.FiscoConnectionString)
 	if err != nil {
 		fmt.Println(err)
-		return "", err
+		return "", time.Now(), err
 	}
 	defer db.Close()
 	tx, err := db.Begin()
 	if err != nil {
 		fmt.Println(err)
-		return "", err
+		return "", time.Now(), err
 	}
 	var status string
+	var fecha time.Time
 	const command string = `declare
-		respuesta varchar2(4000);
+		status varchar2(4000);
+		fecha date;
 		begin
-		:respuesta := siper.pkg_taskman.get_status(:task_id);
+		siper.pkg_taskman.get_status(:task_id,:status,:fecha);
 		end;
 	`
-	_, err = tx.Exec(command, sql.Named("task_id", ID), sql.Named("respuesta", sql.Out{Dest: &status}))
+	_, err = tx.Exec(command, sql.Named("task_id", ID), sql.Named("status", sql.Out{Dest: &status}), sql.Named("fecha", sql.Out{Dest: &fecha}))
 	if err != nil {
 		fmt.Println(err)
-		return "", err
+		return "", time.Now(), err
 	}
 	err = tx.Commit()
 	if err != nil {
 		fmt.Println(err)
-		return "", err
+		return "", time.Now(), err
 	}
-	return status, nil
+	return status, fecha, nil
 }
 
 func setTaskStatus(ID int, status string) (string, error) {
